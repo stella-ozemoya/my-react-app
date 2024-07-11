d4c42c47b04eb136976363391e5d2035fdde20bf
stella-ozemoya <167516349+stella-ozemoya@users.noreply.github.com>
2024-07-12 00:20:10 +0100

    added weather js and css component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react"
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return <div className="App">
+    <div className="container">
+    <Weather />
+    <footer>
+      This project was coded by {" "}
+      <a href="https://weather-forecast-47ew.vercel.app/" target="_blank">
+      Stella Ozemoya
+      </a> {" "}
+      and is {" "} 
+
+      <a 
+      href="https://github.com/stella-ozemoya/my-react-app" 
+      target="_blank">
+
+        open-sourced on GitHub
+        </a>
+    </footer>
+    </div>
+    </div>
 };
 
  
