c3b395857527bafd1509bbee23a45de829333999
stella-ozemoya <167516349+stella-ozemoya@users.noreply.github.com>
2024-07-11 21:14:39 +0100

    Added a github repo URL

--- App.js
+++ App.js
@@ -2,7 +2,23 @@ import React from "react"
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded by {" "}
+      <a href="https://weather-forecast-47ew.vercel.app/" target="_blank">
+      Stella Ozemoya
+      </a> {" "}
+      and is {" "} 
+
+      <a 
+      href="https://github.com/stella-ozemoya/my-react-app" 
+      target="_blank">
+
+        open-sourced on GitHub
+        </a>
+    </footer>
+    </div>
 };
 
  
