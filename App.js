7ffc2120d947659ccac4a429e21d4de3db69c75e
stella-ozemoya <167516349+stella-ozemoya@users.noreply.github.com>
2024-07-11 22:09:48 +0100

    added bootstrap

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 export default function App() {
   return <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
     <footer>
       This project was coded by {" "}
@@ -18,6 +19,7 @@ export default function App() {
         open-sourced on GitHub
         </a>
     </footer>
+    </div>
     </div>
 };
 
