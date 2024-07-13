5306d713c6891ceb4f7e754e0a38b68711010361
stella-ozemoya <167516349+stella-ozemoya@users.noreply.github.com>
2024-07-13 10:39:33 +0100

    added classes to the Weather.js

--- Weather.js
+++ Weather.js
@@ -6,10 +6,13 @@ export default function Weather() {
         <form>
             <div className="row">
             <div className="col-9" >
-            <input type="search" placeholder="Enter a city .." className="form-control" />
+            <input type="search" placeholder="Enter a city .."
+            className="form-control" autoFocus="on" />
             </div>
             <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input type="submit" 
+            value="search" 
+            className="btn btn-primary w-100"/>
             </div>
             </div>
         </form>
@@ -17,13 +20,17 @@ export default function Weather() {
         <ul>
             <li>Wednesday 97:80</li>
             <li>mostly cloudy</li>
-        </ul>
-        <div className="row">
-            <div className="col-6">
+        </ul> 
+    <div className="row mt-3">
+        <div className="col-6">
+            <div className="clearfix">
                 <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy"/>
-                6c
-                </div>
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+                alt="partly cloudy"/>
+                <span className="temperature">6</span>
+                <span className="unit">°c</span>
+                </div>  
+        </div>
                 <div className="col-6">
                     <ul>
                         <li>Precipitation: 15%</li>
@@ -31,7 +38,7 @@ export default function Weather() {
                         <li>Wind: 13km/h</li>
                     </ul>
                 </div>
-        </div>
+    </div>
         
         </div>
 }
